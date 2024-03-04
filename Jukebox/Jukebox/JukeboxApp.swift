809ccde41beae84c2bb91cc08a85e4a4e48a93b8
박성준 <SGO01016950@mobmail.shinhan.com>
2024-03-04 14:30:27 +0900

    Project Init

---
+++ Jukebox/Jukebox/JukeboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  JukeboxApp.swift
+//  Jukebox
+//
+//  Created by Khai on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct JukeboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
